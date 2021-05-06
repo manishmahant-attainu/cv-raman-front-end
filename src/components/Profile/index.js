605887e6b957e47834b0979cbd0a48a2b7c57e76
Manish Mahant <manishmahant>
2021-05-06 09:30:19 +0530

    useEffect Hook

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,100 +1,113 @@
-import { Component } from 'react';
-
+import { useState, useEffect } from 'react';
 import './Profile.css';
-// When this component is rendered display user info with the help of an API
-// Min Req: 1. API  2. A method to call that API
 
-//https://jsonplaceholder.typicode.com/users
+const Profile = () => {
 
-export default class Profile extends Component {
-    state = {
-        time: null,
-        users: [], //to hold all the users
-        userInfo: null, //to hold the info of single user
-        selectedUser: null, //to hold id for selected user
-    }
+  const [state, setState] = useState({
+    time: null,
+    users: [], //to hold all the users
+    userInfo: null, //to hold the info of single user
+    selectedUser: null, //to hold id for selected user
+  });
 
-    fetchUser = (id) => {
-        const path = id ?
-            `https://jsonplaceholder.typicode.com/users/${id}` :
-            'https://jsonplaceholder.typicode.com/users';
-        
-        fetch(path)
-            .then(res=>res.json())
-            .then(data=>{
-                if(id) {
-                    this.setState({userInfo: data})
-                } else {
-                    this.setState({users: data})
-                }
-            })
-    }
+  // For reference
+  // useEffect(()=>{
+  //   console.log('blank dependency array then it works as a componentDidMount');
+  // },[]);
 
-    componentDidMount() {
-        // console.log('Profile componentDidMount');
-        this.fetchUser();
-        this.timer = setInterval(() => {
-            this.setState({time: (new Date()).toLocaleTimeString()})
-        }, 1000);
-    }
+  // useEffect(()=>{
+  //   console.log('no dependency array then it works as a componentDidUpdate');
+  // });
 
-    componentDidUpdate(prevProps, prevState) {
-        if(prevState.selectedUser !== this.state.selectedUser) {
-            this.fetchUser(this.state.selectedUser);
+  // useEffect(()=>{
+  //   console.log(`passed a dependency state.time: ${state.time}`);
+  // },[state.time]);
+
+  // useEffect(()=>{
+  //   console.log(`passed a dependency state.selectedUser: ${state.selectedUser}`);
+  // },[state.selectedUser, state.time]);
+
+
+  const fetchUser = (id) => {
+    const path = id ?
+      `https://jsonplaceholder.typicode.com/users/${id}` :
+      'https://jsonplaceholder.typicode.com/users';
+
+    fetch(path)
+      .then(res => res.json())
+      .then(data => {
+        if (id) {
+          setState((s)=>{
+            return { ...s, userInfo: data }
+          })
+        } else {
+          setState((s)=>({ ...s, users: data }))
         }
-    }
+      })
+  }
 
-    componentWillUnmount() {
-        console.log('Profile componentWillUnmount');
-        clearInterval(this.timer);
-    }
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-    getUserInfo = (id) => {
-        //This is when we have all the data
-        // const userInfo = this.state.users.filter(user => user.id === id);
-        // if(userInfo.length) {
-        //     this.setState({userInfo: userInfo[0]})
-        // }
-        //OR
-        //this is when we have to fetch user info from API
-        // this.fetchUser(id);
-        //OR
-        this.setState({selectedUser:id})
-    }
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log((new Date()).toLocaleTimeString())
+      setState((s)=>({ ...s, time: (new Date()).toLocaleTimeString() }))
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
-    render() {
-        
-        return (
-            <>
-                <h1>Profile: {this.state.time}</h1>
-                <div className="container">
-                <div className="user-container">
-                    <h2>User List</h2>
-                    {
-                        this.state.users.map((user,index) => {
-                            return (
-                                <div
-                                    className="profile-card"
-                                    key={index}
-                                    onClick={() => this.getUserInfo(user.id)}
-                                >
-                                    <h1>{user.name}</h1>
-                                    <address>{user.address.city}</address>
-                                </div>
-                            )
-                        })
-                    }
+  useEffect(() => {
+    if(state.selectedUser) fetchUser(state.selectedUser);
+  }, [state.selectedUser]);
+
+
+  const getUserInfo = (id) => {
+    //This is when we have all the data
+    // const userInfo = state.users.filter(user => user.id === id);
+    // if(userInfo.length) {
+    //     setState({userInfo: userInfo[0]})
+    // }
+    //OR
+    //this is when we have to fetch user info from API
+    // fetchUser(id);
+    //OR
+    setState({ ...state, selectedUser: id })
+  }
+
+
+  return (
+    <>
+      <h1>Profile: {state.time}</h1>
+      <div className="container">
+        <div className="user-container">
+          <h2>User List</h2>
+          {
+            state.users.map((user, index) => {
+              return (
+                <div
+                  className="profile-card"
+                  key={index}
+                  onClick={() => getUserInfo(user.id)}
+                >
+                  <h1>{user.name}</h1>
+                  <address>{user.address.city}</address>
                 </div>
-                
-                <div className="user-info">
-                    <h2>User Info</h2>
-                    <div className="profile-card">
-                        <h1>{this.state.userInfo?.name}</h1>
-                        <address>{this.state.userInfo?.address?.city}</address>
-                    </div>
-                </div>
-            </div>
-            </>
-        )
-    }
+              )
+            })
+          }
+        </div>
+        <div className="user-info">
+          <h2>User Info</h2>
+          <div className="profile-card">
+            <h1>{state.userInfo?.name}</h1>
+            <address>{state.userInfo?.address?.city}</address>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
+
+export default Profile;
