ac9b75b692d9e66872b2123c2887010acc97e330
Manish Mahant <manishmahant>
2021-05-07 08:09:18 +0530

    router starter files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,7 @@
-import { Component } from 'react';
-import Profile from '../components/Profile';
-
-export default class App extends Component {
-    state = {
-        showProfile: true,
-    }
-
-    componentDidMount() {
-        // console.log('componentDidMount');
-    }
-
-    componentDidUpdate() {
-        // console.log('componentDidUpdate');
-    }
-
-    render() {
-        console.log('Render & Re-render')
-        return (
-            <>
-                <h1>Life Cycle Methods</h1>
-                <button
-                    type="button"
-                    onClick={()=>this.setState({showProfile: !this.state.showProfile})}
-                >
-                    Toggle Profile
-                </button>
-                { this.state.showProfile && <Profile />} 
-            </>
-        )
-    }
+const App = () => {
+    return (
+        <h1>Hello</h1>
+    )
 }
+
+export default App;
